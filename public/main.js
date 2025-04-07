bf4142aa12fd793157826c1621e3c12770c7963d
AllanSangle <113636555+AllanSangle@users.noreply.github.com>
2025-04-07 02:48:59 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -5,10 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
     
     // Initialize scores
     const scores = [
-        { name: "A", value: 0 },
-        { name: "B", value: 0 },
-        { name: "C", value: 0 },
-        { name: "D", value: 0 },
+        { name: "ENFP", value: 0 },
+        { name: "INFP", value: 0 },
+        { name: "ISFP", value: 0 },
+        { name: "ESFJ", value: 0 },
+        { name: "INTP", value: 0 },
+        { name: "ESFP", value: 0 },
+        { name: "INFJ", value: 0 },
+        { name: "ENFJ", value: 0 },
+        { name: "ISTJ", value: 0 },
+        { name: "ENTP", value: 0 },
+        { name: "ESTP", value: 0 },
+        { name: "ESTJ", value: 0 },
+        { name: "INTJ", value: 0 },
+        { name: "ISTP", value: 0 },
     ];
     
     // Initialize dialogue index
@@ -20,20 +30,20 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["Let's start with the basics", "1) What is 'love' to you?"],
             choices: [
-                { id: 1, text: "a safe space, to be ourselves and to simply be together", type: 'A', weight: 1, next: 1 },
-                { id: 2, text: "an adventure, filled with fun and unforgettable memories", type: 'B', weight: 1, next: 1 },
-                { id: 3, text: "a commitment, being there for each other no matter what", type: 'C', weight: 1, next: 1 },
-                { id: 4, text: "a beautiful friendship, built on trust and understanding", type: 'D', weight: 1, next: 1 }
+                { id: 1, text: "a safe space, to be ourselves and to simply be together", type: 'INFP', weight: 1, next: 1 },
+                { id: 2, text: "an adventure, filled with fun and unforgettable memories", type: 'ENFP', weight: 1, next: 1 },
+                { id: 3, text: "a commitment, being there for each other no matter what", type: 'ESTJ', weight: 1, next: 1 },
+                { id: 4, text: "a beautiful friendship, built on trust and understanding", type: 'ISTJ', weight: 1, next: 1 }
             ]
         },
         {
             speaker: 'bot',
             text: ["2) What do you value most in a relationship?"],
             choices: [
-                { id: 1, text: "Emotional connection and closeness", type: 'A', weight: 1, next: 2 },
-                { id: 2, text: "Excitement and spontaneity", type: 'B', weight: 1, next: 2 },
-                { id: 3, text: "Loyalty and reliability", type: 'C', weight: 1, next: 2 },
-                { id: 4, text: "Open communication and honesty", type: 'D', weight: 1, next: 2 }
+                { id: 1, text: "Emotional connection and closeness", type: 'ENFP', weight: 1, next: 2 },
+                { id: 2, text: "Excitement and spontaneity", type: 'ESFP', weight: 1, next: 2 },
+                { id: 3, text: "Loyalty and reliability", type: 'INFJ', weight: 1, next: 2 },
+                { id: 4, text: "Open communication and honesty", type: 'INFP', weight: 1, next: 2 }
             ]
         },
         // Add more questions here...
@@ -43,20 +53,20 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["3) Do you regain your energy from being alone in your own safe space or going out and hanging with other people?"], 
             choices: [
-                { id: 1, text: "I hang out with people every chance I get. What’s the fun in staying home on a weekend?", type: 'A', weight: 1, next: 3 }, //E
-                { id: 2, text: "I prefer to recharge in the comfort of my room or favourite corner, maybe listening to some tunes.", type: 'B', weight: 1, next: 3 }, //I
-                { id: 3, text: "I do like being with people, but after a long day of socializing, I need time to myself ", type: 'C', weight: 1, next: 3 }, //I
-                { id: 4, text: "Being with people, to me, is comforting ", type: 'D', weight: 1, next: 3 } //E
+                { id: 1, text: "I hang out with people every chance I get. What’s the fun in staying home on a weekend?", type: 'ENFP', weight: 1, next: 3 }, //ENFP
+                { id: 2, text: "I prefer to recharge in the comfort of my room or favourite corner, maybe listening to some tunes.", type: 'INFP', weight: 1, next: 3 }, //INFP
+                { id: 3, text: "I do like being with people, but after a long day of socializing, I need time to myself ", type: 'ISFP', weight: 1, next: 3 }, //ISFP
+                { id: 4, text: "Being with people, to me, is comforting ", type: 'ESFJ', weight: 1, next: 3 } //ESFJ
             ]
         },
         {
             speaker: 'bot',
             text: ["4) Do you thrive in social settings, or are you just wondering when it’s time to go home?"], 
             choices: [
-                { id: 1, text: "When can I go homee?", type: 'A', weight: 1, next: 4 }, //I
-                { id: 2, text: "Oh yeah, I do consider myself the life of the party! ", type: 'B', weight: 1, next: 4 }, //E
-                { id: 3, text: "If it’s a small gathering over a big party then I’m fine ", type: 'C', weight: 1, next: 4 }, //I
-                { id: 4, text: "A party? To meet new people? Sign me up! ", type: 'D', weight: 1, next: 4 } //E
+                { id: 1, text: "When can I go homee?", type: 'INTP', weight: 1, next: 4 }, //INTP
+                { id: 2, text: "Oh yeah, I do consider myself the life of the party! ", type: 'ESFP', weight: 1, next: 4 }, //ESFP
+                { id: 3, text: "If it’s a small gathering over a big party then I’m fine ", type: 'INFJ', weight: 1, next: 4 }, //INFJ
+                { id: 4, text: "A party? To meet new people? Sign me up! ", type: 'ENFJ', weight: 1, next: 4 } //ENFJ
             ]
         },
 
@@ -64,10 +74,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["5) How long does it take for your social battery to drain?"],
             choices: [
-                { id: 1, text: "Oh boy.  A minute is my maximum.", type: 'A', weight: 1, next: 5 }, //I
-                { id: 2, text: "Never <3 ", type: 'B', weight: 1, next: 5 }, //E
-                { id: 3, text: "Social battery, what’s that? ", type: 'C', weight: 1, next: 5 }, //E
-                { id: 4, text: "A couple of hours or so, give or take? ", type: 'D', weight: 1, next: 5 } //I
+                { id: 1, text: "Oh boy.  A minute is my maximum.", type: 'ISTJ', weight: 1, next: 5 }, //ISTJ
+                { id: 2, text: "Never <3 ", type: 'ENTP', weight: 1, next: 5 }, //ENTP
+                { id: 3, text: "Social battery, what’s that? ", type: 'ESTP', weight: 1, next: 5 }, //ESTP
+                { id: 4, text: "A couple of hours or so, give or take? ", type: 'ISFJ', weight: 1, next: 5 } //ISFJ
             ]
         },
 
@@ -77,10 +87,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["6) When working on a new project, do you rely on proven facts to determine your course of action enjoy coming up with your own ideas?"],
             choices: [
-                { id: 1, text: "It’s all in the facts.", type: 'A', weight: 1, next: 6 }, //S
-                { id: 2, text: "Let your own ideas shine!", type: 'B', weight: 1, next: 6 }, //N
-                { id: 3, text: "I think being able to rely on something proven is nice.", type: 'C', weight: 1, next: 6 }, //S
-                { id: 4, text: "It’s more fun to create something new and exciting.", type: 'D', weight: 1, next: 6 } //N
+                { id: 1, text: "It’s all in the facts.", type: 'ESTJ', weight: 1, next: 6 }, //ESTJ
+                { id: 2, text: "Let your own ideas shine!", type: 'INFP', weight: 1, next: 6 }, //INFP
+                { id: 3, text: "I think being able to rely on something proven is nice.", type: 'ISTJ', weight: 1, next: 6 }, //ISTJ
+                { id: 4, text: "It’s more fun to create something new and exciting.", type: 'ENFP', weight: 1, next: 6 } //ENFP
             ]
         },
 
@@ -88,10 +98,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["7) Do you tend to skip over the details, or do you often find yourself overthinking each one? "],
             choices: [
-                { id: 1, text: "Wait, was the question again? ", type: 'A', weight: 1, next: 7 }, //N
-                { id: 2, text: "Let me think about it. ", type: 'B', weight: 1, next: 7 }, //S
-                { id: 3, text: "What’s the fun in being bogged down by details? ", type: 'C', weight: 1, next: 7 }, //N
-                { id: 4, text: "Details are key. ", type: 'D', weight: 1, next: 7 } //S
+                { id: 1, text: "Wait, was the question again? ", type: 'INFP', weight: 1, next: 7 }, //INFP
+                { id: 2, text: "Let me think about it. ", type: 'ISTJ', weight: 1, next: 7 }, //ISTJ
+                { id: 3, text: "What’s the fun in being bogged down by details? ", type: 'ENFP', weight: 1, next: 7 }, //ENFP
+                { id: 4, text: "Details are key. ", type: 'INTJ', weight: 1, next: 7 } //ISFJ
             ]
         },
 
@@ -99,10 +109,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["8) Do you focus more on the present and your current situation, or the bigger picture, seeking the potential in things?"],
             choices: [
-                { id: 1, text: "Think big, make it big.", type: 'A', weight: 1, next: 8 }, //N
-                { id: 2, text: "Deal with the situation at hand first.", type: 'B', weight: 1, next: 8 }, //S
-                { id: 3, text: "I do love the thrill of spotting potential", type: 'C', weight: 1, next: 8 }, //N
-                { id: 4, text: "The present is what matters most importantly", type: 'D', weight: 1, next: 8 } //S
+                { id: 1, text: "Think big, make it big.", type: 'ENTP', weight: 1, next: 8 }, //ENTP
+                { id: 2, text: "Deal with the situation at hand first.", type: 'ESTJ', weight: 1, next: 8 }, //ESTJ
+                { id: 3, text: "I do love the thrill of spotting potential", type: 'INFJ', weight: 1, next: 8 }, //INFJ
+                { id: 4, text: "The present is what matters most importantly", type: 'ESFJ', weight: 1, next: 8 } //ESFJ
             ]
         },
 
@@ -112,10 +122,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["9) Would you identify yourself as a logical person or one who values emotions and personal values? "],
             choices: [
-                { id: 1, text: "Emotions are what make us human! ", type: 'A', weight: 1, next: 9 }, //F
-                { id: 2, text: "Staying logical is the safest bet.", type: 'B', weight: 1, next: 9 }, //T
-                { id: 3, text: "I like to make decisions based on my personal values ", type: 'C', weight: 1, next: 9 }, //F
-                { id: 4, text: "Brain over heart any day. ", type: 'D', weight: 1, next: 9 } //T
+                { id: 1, text: "Emotions are what make us human! ", type: 'ENFP', weight: 1, next: 9 }, //ENFP
+                { id: 2, text: "Staying logical is the safest bet.", type: 'INTJ', weight: 1, next: 9 }, //INTJ
+                { id: 3, text: "I like to make decisions based on my personal values ", type: 'ISFP', weight: 1, next: 9 }, //ISFP
+                { id: 4, text: "Brain over heart any day. ", type: 'ISTP', weight: 1, next: 9 } //ISTP
             ]
         },
 
@@ -123,10 +133,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["10) Do you get clouded by your emotions often, or are you more grounded?"],
             choices: [
-                { id: 1, text: "My mind goes on rollercoasters of emotions ", type: 'A', weight: 1, next: 10 }, //F
-                { id: 2, text: "I tend to put my own emotions to the side for the greater good.", type: 'B', weight: 1, next: 10 }, //T
-                { id: 3, text: "Being reasonable is what got me this far. ", type: 'C', weight: 1, next: 10 }, //T
-                { id: 4, text: "I let my emotions take the driver's seat too often", type: 'D', weight: 1, next: 10 } //F
+                { id: 1, text: "My mind goes on rollercoasters of emotions ", type: 'INFP', weight: 1, next: 10 }, //INFP
+                { id: 2, text: "I tend to put my own emotions to the side for the greater good.", type: 'INTP', weight: 1, next: 10 }, //INTP
+                { id: 3, text: "Being reasonable is what got me this far. ", type: 'ESTJ', weight: 1, next: 10 }, //ESTJ
+                { id: 4, text: "I let my emotions take the driver's seat too often", type: 'ENFJ', weight: 1, next: 10 } //ENFJ
             ]
         },
 
@@ -134,10 +144,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["11) When someone comes to you with a problem, what’s your first instinct?"],
             choices: [
-                { id: 1, text: "I find a practical solution ", type: 'A', weight: 1, next: 11 }, //T
-                { id: 2, text: "I listen and empathize, it’s best to allow people to be heard", type: 'B', weight: 1, next: 11 }, //F
-                { id: 3, text: "I get too emotionally invested", type: 'C', weight: 1, next: 11 }, //F
-                { id: 4, text: "I try piecing out where it went wrong in the first place", type: 'D', weight: 1, next: 11 } //T
+                { id: 1, text: "I find a practical solution ", type: 'ISTJ', weight: 1, next: 11 }, //ISTJ
+                { id: 2, text: "I listen and empathize, it’s best to allow people to be heard", type: 'ENFJ', weight: 1, next: 11 }, //ENFJ
+                { id: 3, text: "I get too emotionally invested", type: 'ISFP', weight: 1, next: 11 }, //ISFP
+                { id: 4, text: "I try piecing out where it went wrong in the first place", type: 'INTJ', weight: 1, next: 11 } //INTJ
             ]
         },
 
@@ -147,10 +157,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["12) How often do you like to stick to a schedule?"],
             choices: [
-                { id: 1, text: "I can’t think of my day-to-day without a schedule", type: 'A', weight: 1, next: 12 }, //J
-                { id: 2, text: "Oh, I avoid schedules like the plague! ", type: 'B', weight: 1, next: 12 }, //P
-                { id: 3, text: "I try creating schedules, but they never work out too much", type: 'C', weight: 1, next: 12 }, //P
-                { id: 4, text: "I like schedules. They are concrete and reliable", type: 'D', weight: 1, next: 12 } //J
+                { id: 1, text: "I can’t think of my day-to-day without a schedule", type: 'INFJ', weight: 1, next: 12 }, //INFJ
+                { id: 2, text: "Oh, I avoid schedules like the plague! ", type: 'ESFP', weight: 1, next: 12 }, //ESFP
+                { id: 3, text: "I try creating schedules, but they never work out too much", type: 'INFP', weight: 1, next: 12 }, //INFP
+                { id: 4, text: "I like schedules. They are concrete and reliable", type: 'ESTJ', weight: 1, next: 12 } //ESTJ
             ]
         },
 
@@ -158,10 +168,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["13) Do you consider yourself open-minded about most things?"],
             choices: [
-                { id: 1, text: "My mind is as open as space!", type: 'A', weight: 1, next: 13 }, //P
-                { id: 2, text: "I do tend to have my own opinions, but I keep them to myself for the most part", type: 'B', weight: 1, next: 13 }, //J
-                { id: 3, text: "Yup! I think so, I love learning new things.", type: 'C', weight: 1, next: 13 }, //P
-                { id: 4, text: "Nope. My opinion is the only right opinion.", type: 'D', weight: 1, next: 13 } //J
+                { id: 1, text: "My mind is as open as space!", type: 'ENFP', weight: 1, next: 13 }, //ENFP
+                { id: 2, text: "I do tend to have my own opinions, but I keep them to myself for the most part", type: 'ISTJ', weight: 1, next: 13 }, //ISTJ
+                { id: 3, text: "Yup! I think so, I love learning new things.", type: 'INFP', weight: 1, next: 13 }, //INFP
+                { id: 4, text: "Nope. My opinion is the only right opinion.", type: 'ENTJ', weight: 1, next: 13 } //ENTJ
             ]
         },
 
@@ -169,10 +179,10 @@ document.addEventListener('DOMContentLoaded', function () {
             speaker: 'bot',
             text: ["14) How much do you like surprises? "],
             choices: [
-                { id: 1, text: "Ooooh, are you giving me a surprise? Exciting! ", type: 'A', weight: 1, next: 14 }, //P
-                { id: 2, text: "From time to time.", type: 'B', weight: 1, next: 14 }, //J
-                { id: 3, text: "Not much, it messes with my plans sometimes.", type: 'C', weight: 1, next: 14 }, //J
-                { id: 4, text: "Opening up gifts was my favourite part of Christmas ", type: 'D', weight: 1, next: 14 } //P
+                { id: 1, text: "Ooooh, are you giving me a surprise? Exciting! ", type: 'ESFP', weight: 1, next: 14 }, //ESFP
+                { id: 2, text: "From time to time.", type: 'ISFJ', weight: 1, next: 14 }, //ISFJ
+                { id: 3, text: "Not much, it messes with my plans sometimes.", type: 'INTJ', weight: 1, next: 14 }, //INTJ
+                { id: 4, text: "Opening up gifts was my favourite part of Christmas ", type: 'ENFP', weight: 1, next: 14 } //ENFP
             ]
         },
 
@@ -442,3 +452,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     
 });
+
+function talkToPersonality() {
+    window.location.href = '/chat.html';  // Redirects the user to chat.html
+}
